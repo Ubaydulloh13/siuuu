1f701193b609b14392d4bef4211af4c486fb5d82
Ubaydulloh13 <samushakirov@gmail.com>
2025-08-25 11:53:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const body=document.getElementsByTagName("body")[0];
+const p=document.getElementById("text");
+
+
+const rang1=prompt("Body rangini kiriting");
+const rang2=prompt("Yozuv rangini kiriting");
+
+body.style.backgroundColor=rang1;
+p.style.color=rang2;
